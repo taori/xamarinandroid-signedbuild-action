027c095458c86e2915962f3a7a232dab0fa96da6
Andreas Müller <taori@users.noreply.github.com>
2021-07-23 23:18:52 +0200

    Update build-android.sh

--- build-android.sh
+++ build-android.sh
@@ -49,8 +49,8 @@ then
     CONFIGURATION=Release
 fi
 
-encoded_alias = printf "%%<%s>" $(od -A n -t x1<<<"$ALIAS")
-encoded_signing_key = printf "%%<%s>" $(od -A n -t x1<<<"$SIGNING_KEY_PASS")
-encoded_key_store_password = printf "%%<%s>" $(od -A n -t x1<<<"$KEY_STORE_PASSWORD")
+ENCODED_ALIAS = printf "%%<%s>" $(od -A n -t x1<<<"$ALIAS")
+ENCODED_SIGNING_KEY_PASS = printf "%%<%s>" $(od -A n -t x1<<<"$SIGNING_KEY_PASS")
+ENCODED_KEY_STORE_PASSWORD = printf "%%<%s>" $(od -A n -t x1<<<"$KEY_STORE_PASSWORD")
 
-msbuild "$CSPROJ_PATH" /t:restore /verbosity:normal /t:Rebuild /t:SignAndroidPackage /p:Configuration="$CONFIGURATION" /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias="$encoded_alias" /p:AndroidSigningKeyPass="$encoded_signing_key" /p:AndroidSigningKeyStore="$RUNNER_TEMP"/android.keystore /p:AndroidSigningStorePass="$encoded_key_store_password"
+msbuild "$CSPROJ_PATH" /t:restore /verbosity:normal /t:Rebuild /t:SignAndroidPackage /p:Configuration="$CONFIGURATION" /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias="$ENCODED_ALIAS" /p:AndroidSigningKeyPass="$ENCODED_SIGNING_KEY_PASS" /p:AndroidSigningKeyStore="$RUNNER_TEMP"/android.keystore /p:AndroidSigningStorePass="$ENCODED_KEY_STORE_PASSWORD"
