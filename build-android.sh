a025c4d1c82d9ed4ccaaec3fd6c6d2063f28185f
Andreas Müller <taori@users.noreply.github.com>
2021-07-23 22:50:00 +0200

    Update build-android.sh

--- build-android.sh
+++ build-android.sh
@@ -49,4 +49,8 @@ then
     CONFIGURATION=Release
 fi
 
-msbuild "$CSPROJ_PATH" /t:restore /verbosity:normal /t:Rebuild /t:SignAndroidPackage /p:Configuration="$CONFIGURATION" /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias="$ALIAS" /p:AndroidSigningKeyPass="$SIGNING_KEY_PASS" /p:AndroidSigningKeyStore="$RUNNER_TEMP"/android.keystore /p:AndroidSigningStorePass="$KEY_STORE_PASSWORD"
+encoded_alias = printf "%%<%s>" $(od -A n -t x1<<<"$ALIAS")
+encoded_signing_key = printf "%%<%s>" $(od -A n -t x1<<<"$SIGNING_KEY_PASS")
+encoded_key_store_password = printf "%%<%s>" $(od -A n -t x1<<<"$KEY_STORE_PASSWORD")
+
+msbuild "$CSPROJ_PATH" /t:restore /verbosity:normal /t:Rebuild /t:SignAndroidPackage /p:Configuration="$CONFIGURATION" /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias="$encoded_alias" /p:AndroidSigningKeyPass="$encoded_signing_key" /p:AndroidSigningKeyStore="$RUNNER_TEMP"/android.keystore /p:AndroidSigningStorePass="$encoded_key_store_password"
